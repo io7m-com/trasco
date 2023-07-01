e227678bff6b903304c84bd7e3cdd58fccd7c093
Mark Raynsford <code@io7m.com>
2023-07-01 13:08:10 +0000

    Update primogenitor, dependencies, move test sources root.

---
+++ com.io7m.trasco.tests/src/main/java/module-info.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright © 2023 Mark Raynsford <code@io7m.com> https://www.io7m.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * SQL revision system (Test suite)
+ */
+
+open module com.io7m.trasco.tests
+{
+  requires org.junit.jupiter.api;
+  requires org.junit.jupiter.engine;
+  requires org.junit.platform.commons;
+  requires org.junit.platform.engine;
+
+  requires com.io7m.trasco.api;
+  requires com.io7m.trasco.vanilla;
+  requires org.apache.derby.tools;
+  requires org.slf4j;
+
+  exports com.io7m.trasco.tests;
+}
